ace15b2dfe7f4ce176ce101098e6c5539231cae7
Ghaza <akmalkay12@gmail.com>
2022-04-05 22:22:13 +0700

    Upload Splash Screen

---
+++ lib/splash.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({required this.nextScreen, Key? key}) : super(key: key);
+
+  final Widget nextScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: const Text(
+        "Anlyzer",
+        style: TextStyle(
+          fontSize: 35,
+          color: Colors.white,
+          fontFamily: 'OpenSans',
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      nextScreen: nextScreen,
+      backgroundColor: const Color.fromARGB(255, 35, 151, 243),
+      splashTransition: SplashTransition.fadeTransition,
+      animationDuration: const Duration(milliseconds: 700),
+      duration: 1800,
+    );
+  }
+}
